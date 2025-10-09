78a0cb2c7df2943ad8fad850dbf448b8ed8f2233
Niels Kjeldbjerg <353468@viauc.dk>
2025-10-09 14:16:43 +0200

    commit (grpc in rider)

--- app-server/Program.cs
+++ app-server/Program.cs
@@ -1,4 +1,5 @@
 using Grpc.Net.Client;
+using Studhub.Grpc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-//builder.Services.AddGrpcClient
+/**
+builder.Services.AddGrpcClient<>(o =>
+{
+    o.Address = new Uri("https://localhost:9090");
+});
+**/
 
 var app = builder.Build();
 
