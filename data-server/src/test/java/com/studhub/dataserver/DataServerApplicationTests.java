49ca5e4ccb3e04b10cf05f05d4a27826eabaf961
Niels Kjeldbjerg <353468@viauc.dk>
2025-10-09 13:29:10 +0200

    dataserver init commit

---
+++ data-server/src/test/java/com/studhub/dataserver/DataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studhub.dataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
